f3d75384859d1d2b11907fe2c363880271fa543c
hyejooLim <lhyejoo@naver.com>
2021-08-11 14:34:53 +0900

    Add comment

--- components/Home.js
+++ components/Home.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { View, Text, Button } from 'react-native';
 
+// navigation: 모든 screen이 가지는 prop
 const Home = ({ navigation }) => {
   return (
     <View>
@@ -8,6 +9,6 @@ const Home = ({ navigation }) => {
       <Button title="Go to Detail" onPress={() => navigation.navigate('Detail')} />
     </View>
   );
-};
+}; 
 
 export default Home;
