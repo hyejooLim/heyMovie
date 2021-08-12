579bce6a84880a8a6de2799e5a04812aa5ac4dda
hyejooLim <lhyejoo@naver.com>
2021-08-12 17:41:33 +0900

    Feat: get tv data

--- screens/TV.js
+++ screens/TV.js
@@ -1,10 +1,43 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { View, Text } from 'react-native';
 
+import { tvApi } from '../api';
+
 const TV = () => {
+  const [tv, setTV] = useState({
+    today: [],
+    todayError: null,
+    weekday: [],
+    weekdayError: null,
+    popular: [],
+    popularError: null,
+    topRated: [],
+    topRatedError: null,
+  });
+  const getData = async () => {
+    const [today, todayError] = await tvApi.today();
+    const [weekday, weekdayError] = await tvApi.weekday();
+    const [popular, popularError] = await tvApi.popular();
+    const [topRated, topRatedError] = await tvApi.topRated();
+    setTV({
+      today,
+      todayError,
+      weekday,
+      weekdayError,
+      popular,
+      popularError,
+      topRated,
+      topRatedError,
+    });
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
+
   return (
     <View>
-      <Text>드라마</Text>
+      <Text>{tv.today?.length}</Text>
     </View>
   );
 };
