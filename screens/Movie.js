26fbb53977013e30937105b3b4bc78f13444af44
hyejooLim <lhyejoo@naver.com>
2021-08-12 17:28:25 +0900

    Feat: get movie data

--- screens/Movie.js
+++ screens/Movie.js
@@ -1,11 +1,38 @@
-import React from 'react';
-import { View, Text, Button } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { View, Text } from 'react-native';
 
-const Movie = ({ navigation }) => {
+import { movieApi } from '../api';
+
+const Movie = () => {
+  const [movies, setMovies] = useState({
+    nowPlaying: [],
+    nowPlayingError: null,
+    popular: [],
+    popularError: null,
+    upcoming: [],
+    upcomingError: null,
+  });
+  const getData = async () => {
+    const [nowPlaying, nowPlayingError] = await movieApi.nowPlaying();
+    const [popular, popularError] = await movieApi.popular();
+    const [upcoming, upcomingError] = await movieApi.upcoming();
+    setMovies({
+      nowPlaying,
+      nowPlayingError,
+      popular,
+      popularError,
+      upcoming,
+      upcomingError,
+    });
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
+
   return (
-    <View style={{ flex: 1, backgroundColor: "black" }}>
-      <Text>영화</Text>
-      <Button title="Movie" onPress={() => { navigation.navigate('Detail') }} />
+    <View style={{ flex: 1, backgroundColor: 'black' }}>
+      <Text style={{ color: 'white' }}>{movies.nowPlaying?.length}</Text>
     </View>
   );
 };
