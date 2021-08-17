eb079029491d4198776a8c578c50452289ecd07b
hyejooLim <lhyejoo@naver.com>
2021-08-17 22:43:52 +0900

    Modify: code style

---
+++ components/Slider.js
@@ -0,0 +1,98 @@
+import React from 'react';
+import { TouchableOpacity } from 'react-native';
+import PropTypes from 'prop-types';
+import styled from 'styled-components/native';
+
+import { getImageUrl } from '../api';
+import { trimText } from './utils';
+import Poster from './Poster';
+
+const Container = styled.View`
+  width: 100%;
+  height: 100%;
+`;
+
+const BackgroundImage = styled.Image`
+  width: 100%;
+  height: 100%;
+  opacity: 0.4;
+  position: absolute;
+`;
+
+const Content = styled.View`
+  height: 100%;
+  flex-direction: row;
+  justify-content: space-around;
+  align-items: center;
+`;
+
+const Description = styled.View`
+  width: 60%;
+  align-items: flex-start;
+`;
+
+const Title = styled.Text`
+  font-size: 18px;
+  font-weight: 600;
+  color: white;
+  margin-bottom: 8px;
+`;
+
+const Votes = styled.Text`
+  color: white;
+  font-size: 12px;
+  opacity: 0.7;
+  margin-bottom: 8px;
+`;
+
+const Overview = styled.Text`
+  color: white;
+  opacity: 0.7;
+  font-weight: 500;
+  text-align: justify;
+  margin-bottom: 8px;
+`;
+
+const Button = styled.View`
+  background: #50AAF3;
+  padding: 6px 10px;
+  border-radius: 4px;
+`;
+
+const Text = styled.Text`
+  color: white;
+`;
+
+const Slider = ({ id, title, bgImage, poster, votes, overview }) => {
+  const bgImageUrl = getImageUrl(bgImage);
+
+  return (
+    <Container>
+      <BackgroundImage source={{ uri: bgImageUrl }} />
+      <Content>
+        <Poster path={poster} />
+        <Description>
+          <Title>{trimText(title, 40)}</Title>
+          <Votes>★ {votes} / 10</Votes>
+          <Overview>{trimText(overview, 120)}</Overview>
+          <TouchableOpacity>
+            <Button>
+              <Text>See More</Text>
+            </Button>
+          </TouchableOpacity>
+        </Description>
+      </Content>
+    </Container>
+  );
+};
+
+Slider.propTypes = {
+  id: PropTypes.number.isRequired,
+  title: PropTypes.string.isRequired,
+  bgImage: PropTypes.string.isRequired,
+  poster: PropTypes.string.isRequired,
+  votes: PropTypes.number.isRequired,
+  overview: PropTypes.string.isRequired,
+};
+
+export default Slider;
--- screens/Movies/MoviePresenter.js
+++ screens/Movies/MoviePresenter.js
@@ -3,7 +3,7 @@ import { Dimensions } from 'react-native';
 import Swiper from 'react-native-web-swiper';
 import styled from 'styled-components/native';
 
-import Slider from '../../components/Movies/Slider';
+import Slider from '../../components/Slider';
 import Title from '../../components/Title';
 import Horizontal from '../../components/Horizontal';
 import Vertical from '../../components/Vertical';
@@ -18,55 +18,51 @@ const SliderWrapper = styled.View`
   margin-bottom: 50px;
 `;
 
-const Container = styled.View`
-  /* margin: 0 20px; */
-`;
+const Container = styled.View``;
 
 const MoviePresenter = ({ loading, nowPlaying, popular, upcoming }) => {
   return (
     <ScreenContainer loading={loading}>
-      <>
-        <SliderWrapper>
-          <Swiper controlsEnabled={false} loop timeout={3}>
-            {nowPlaying.map((movie) => (
-              <Slider
-                key={movie.id}
-                id={movie.id}
-                title={movie.original_title}
-                bgImage={movie.backdrop_path}
-                poster={movie.poster_path}
-                votes={movie.vote_average}
-                overview={movie.overview}
-              />
-            ))}
-          </Swiper>
-        </SliderWrapper>
-        <Container>
-          <HorizontalSlider title={'Popular Movies'}>
-            {popular.map((movie) => (
-              <Horizontal
-                key={movie.id}
-                id={movie.id}
-                poster={movie.poster_path}
-                title={movie.title}
-                votes={movie.vote_average}
-              />
-            ))}
-          </HorizontalSlider>
-          <Title title={'Coming Soon'} />
-          {upcoming.map((movie) => (
-            <Vertical
+      <SliderWrapper>
+        <Swiper controlsEnabled={false} loop timeout={3}>
+          {nowPlaying.map((movie) => (
+            <Slider
               key={movie.id}
               id={movie.id}
+              title={movie.original_title}
+              bgImage={movie.backdrop_path}
               poster={movie.poster_path}
-              title={movie.title}
-              date={movie.release_date}
               votes={movie.vote_average}
               overview={movie.overview}
             />
           ))}
-        </Container>
-      </>
+        </Swiper>
+      </SliderWrapper>
+      <Container>
+        <HorizontalSlider title={'Popular Movies'}>
+          {popular.map((movie) => (
+            <Horizontal
+              key={movie.id}
+              id={movie.id}
+              poster={movie.poster_path}
+              title={movie.title}
+              votes={movie.vote_average}
+            />
+          ))}
+        </HorizontalSlider>
+        <Title title={'Coming Soon'} />
+        {upcoming.map((movie) => (
+          <Vertical
+            key={movie.id}
+            id={movie.id}
+            poster={movie.poster_path}
+            title={movie.title}
+            date={movie.release_date}
+            votes={movie.vote_average}
+            overview={movie.overview}
+          />
+        ))}
+      </Container>
     </ScreenContainer>
   );
 };
