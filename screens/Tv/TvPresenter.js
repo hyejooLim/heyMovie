ba279431a1db7ffd07a7079303a7132065e98d7e
hyejooLim <lhyejoo@naver.com>
2021-08-17 22:47:13 +0900

    Add: today shows slider

--- screens/Tv/TvPresenter.js
+++ screens/Tv/TvPresenter.js
@@ -1,30 +1,44 @@
 import React from 'react';
+import { Dimensions } from 'react-native';
 import styled from 'styled-components/native';
+import Swiper from 'react-native-web-swiper';
 
 import ScreenContainer from '../../components/ScreenContainer';
+import Slider from '../../components/Slider';
 import HorizontalSlider from '../../components/HorizontalSlider';
 import Horizontal from '../../components/Horizontal';
+import Vertical from '../../components/Vertical';
+import Title from '../../components/Title';
 
-const TvWrapper = styled.View`
+const { width, height } = Dimensions.get('window');
+
+const SliderWrapper = styled.View`
   width: 100%;
-  height: 100%;
+  height: ${height / 4}px;
+  margin-bottom: 50px;
 `;
+
+const Container = styled.View``;
 
 const TvPresenter = ({ loading, today, thisweek, popular, topRated }) => {
   return (
-    <TvWrapper>
-      <ScreenContainer loading={loading}>
-        <HorizontalSlider title='Today Shows'>
+    <ScreenContainer loading={loading}>
+      <SliderWrapper>
+        <Swiper controlsEnabled={false} loop timeout={3}>
           {today.map((show) => (
-            <Horizontal
+            <Slider
               key={show.id}
               id={show.id}
-              poster={show.poster_path}
               title={show.name}
+              bgImage={show.backdrop_path}
+              poster={show.poster_path}
               votes={show.vote_average}
+              overview={show.overview}
             />
           ))}
-        </HorizontalSlider>
+        </Swiper>
+      </SliderWrapper>
+      <Container>
         <HorizontalSlider title='Popular Shows'>
           {popular.map((show) => (
             <Horizontal
@@ -36,8 +50,30 @@ const TvPresenter = ({ loading, today, thisweek, popular, topRated }) => {
             />
           ))}
         </HorizontalSlider>
-      </ScreenContainer>
-    </TvWrapper>
+        <HorizontalSlider title='Top Rated Shows'>
+          {topRated.map((show) => (
+            <Horizontal
+              key={show.id}
+              id={show.id}
+              poster={show.poster_path}
+              title={show.name}
+              votes={show.vote_average}
+            />
+          ))}
+        </HorizontalSlider>
+        <Title title={'This Week'} />
+        {thisweek.map((show) => (
+          <Vertical
+            key={show.id}
+            id={show.id}
+            poster={show.poster_path}
+            title={show.name}
+            votes={show.vote_average}
+            overview={show.overview}
+          />
+        ))}
+      </Container>
+    </ScreenContainer>
   );
 };
 
