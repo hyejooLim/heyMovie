052f0cb081bc8cdc6b19eefc6bd42e2df0b8da71
hyejooLim <lhyejoo@naver.com>
2021-08-18 16:20:48 +0900

    Add: HorizontalSlider to search screen

--- screens/Search/SearchContainer.js
+++ screens/Search/SearchContainer.js
@@ -1,20 +1,38 @@
-import React, { useState, useCallback } from 'react';
+import React, { useState } from 'react';
 
+import { movieApi, tvApi } from '../../api';
 import SearchPresenter from './SearchPresenter';
 
 const Search = () => {
   const [value, setValue] = useState('');
+  const [result, setResult] = useState({
+    loading: true,
+    searchMovie: [],
+    searchMovieError: null,
+    searchTv: [],
+    searchTvError: null,
+  });
 
-  const onChangeInput = useCallback((text) => {
+  const onChangeInput = (text) => {
     setValue(text);
-  }, []);
+  };
 
-  const onSubmitBtn = useCallback(() => {
-    console.log(`Enter Button ${value}`);
-  }, [value]);
+  const onSubmitBtn = async () => {
+    const [searchMovie, searchMovieError] = await movieApi.search(value);
+    const [searchTv, searchTvError] = await tvApi.search(value);
+
+    setResult({
+      loading: false,
+      searchMovie,
+      searchMovieError,
+      searchTv,
+      searchTvError,
+    });
+  };
 
   return (
     <SearchPresenter
+      {...result}
       value={value}
       onChange={onChangeInput}
       onSubmit={onSubmitBtn}
--- screens/Search/SearchPresenter.js
+++ screens/Search/SearchPresenter.js
@@ -2,12 +2,27 @@ import React from 'react';
 import styled from 'styled-components/native';
 
 import Input from '../../components/Input';
+import HorizontalSlider from '../../components/HorizontalSlider';
+import Horizontal from '../../components/Horizontal';
 
 const Container = styled.ScrollView`
   background: black;
 `;
 
-const SearchPresenter = ({ value, onChange, onSubmit }) => {
+const Text = styled.Text`
+  color: white;
+  opacity: 0.7;
+  margin: 30px 0 50px 30px;
+`;
+
+const SearchPresenter = ({
+  value,
+  onChange,
+  onSubmit,
+  loading,
+  searchMovie,
+  searchTv,
+}) => {
   return (
     <Container>
       <Input
@@ -16,6 +31,33 @@ const SearchPresenter = ({ value, onChange, onSubmit }) => {
         onChange={onChange}
         onSubmit={onSubmit}
       />
+      {loading ? null : <Text>{value}에 대한 검색 결과입니다.</Text>}
+      {searchMovie.length && (
+        <HorizontalSlider title={'Movies'}>
+          {searchMovie.map((movie) => (
+            <Horizontal
+              key={movie.id}
+              id={movie.id}
+              poster={movie.poster_path}
+              title={movie.title}
+              votes={movie.vote_average}
+            />
+          ))}
+        </HorizontalSlider>
+      )}
+      {searchTv.length && (
+        <HorizontalSlider title={'TV Shows'}>
+          {searchTv.map((show) => (
+            <Horizontal
+              key={show.id}
+              id={show.id}
+              poster={show.poster_path}
+              title={show.name}
+              votes={show.vote_average}
+            />
+          ))}
+        </HorizontalSlider>
+      )}
     </Container>
   );
 };
